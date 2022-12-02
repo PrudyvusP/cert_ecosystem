d22ad4899e89e5bde14570f61ea99fc7e59695c3
rofel <vgolosyandriya@yandex.ru>
2022-12-02 15:34:26 +0300

    some issues with setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 python3 -m venv venv;
 source venv/bin/activate && pip3 install -r requirements.txt;
-echo -e "FLASK_APP=organizations\nFLASK_ENV=testing\nSECRET_KEY=dont_forget_to_change_me_bro" > organizations/.env;
-flask db upgrade && echo "setup has been completed"
+echo -e "FLASK_APP=organizations\nFLASK_ENV=testing\nSECRET_KEY=dont_forget_to_change_me_bro" > .env;
+mv .env organizations/;
+flask db upgrade && echo "setup has been completed";
+python3 fill_demo_data.py;
