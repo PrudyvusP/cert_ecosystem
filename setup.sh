433ded15897288bcd2cbd188585b1a70d582a7d5
rofel <vgolosyandriya@yandex.ru>
2022-12-02 12:26:27 +0300

    setup.sh modified

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
-python3 -m venv venv;
-source venv/bin/activate;
-pip3 install -r requirements.txt;
+cd cert_ecosystem || python3 -m venv venv;
+source venv/bin/activate || pip3 install -r requirements.txt;
 echo -e "FLASK_APP=organizations\nFLASK_ENV=development\nSECRET_KEY=dont_forget_to_change_me_bro" > .env;
 flask db upgrade || echo "setup has been completed"
