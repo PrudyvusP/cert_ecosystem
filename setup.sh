d9d9a487211e818b66ee79dc1cfd961f419b2e0d
rofel <vgolosyandriya@yandex.ru>
2022-12-02 14:23:50 +0300

    README modified and a preparation for gunicorn made

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 python3 -m venv venv;
 source venv/bin/activate && pip3 install -r requirements.txt;
-echo -e "FLASK_APP=organizations\nFLASK_ENV=development\nSECRET_KEY=dont_forget_to_change_me_bro" > .env;
+echo -e "FLASK_APP=organizations\nFLASK_ENV=testing\nSECRET_KEY=dont_forget_to_change_me_bro" > .env;
 flask db upgrade && echo "setup has been completed"
