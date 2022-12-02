57d99ec92d544713bb4adc47b9a519b18a7920ff
rofel <vgolosyandriya@yandex.ru>
2022-12-02 12:28:12 +0300

    cd command deleted from setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-cd cert_ecosystem || python3 -m venv venv;
+python3 -m venv venv;
 source venv/bin/activate || pip3 install -r requirements.txt;
 echo -e "FLASK_APP=organizations\nFLASK_ENV=development\nSECRET_KEY=dont_forget_to_change_me_bro" > .env;
 flask db upgrade || echo "setup has been completed"
