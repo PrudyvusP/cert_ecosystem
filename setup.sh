9b130bee53328cafc5b45259ae35bda990933c58
rofel <vgolosyandriya@yandex.ru>
2022-12-02 12:36:15 +0300

    setup.sh logic with shell && fixed omg

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 python3 -m venv venv;
-source venv/bin/activate || pip3 install -r requirements.txt;
+source venv/bin/activate && pip3 install -r requirements.txt;
 echo -e "FLASK_APP=organizations\nFLASK_ENV=development\nSECRET_KEY=dont_forget_to_change_me_bro" > .env;
-flask db upgrade || echo "setup has been completed"
+flask db upgrade && echo "setup has been completed"
